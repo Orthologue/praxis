97944456fa4075f44b17ae5e704f4dce06c2889f
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-04-07 17:44:14 -0700

    praxis: added package bootstrapping

--- praxis/__init__.py
+++ praxis/__init__.py
@@ -6,8 +6,72 @@
 #
 
 
-# import and publish pyre symbols
+# externals
+import os
 
+
+# geography
+def home():
+    """
+    Return the package installation directory
+    """
+    return os.path.dirname(__file__)
+
+def prefix():
+    """
+    Compute the location of the {praxis} installation
+    """
+    return os.path.abspath(os.path.join(home(), os.path.pardir, os.path.pardir))
+
+def defaults():
+    """
+    Compute the location of the package configuration files
+    """
+    return os.path.abspath(os.path.join(prefix(), 'defaults'))
+
+    
+# convenience for interactive explorations
+def pg():
+    # build a db component
+    dbc = db.postgres(name="praxis:db")
+    # establish the connection
+    dbc.attach()
+    # and return the component
+    return dbc
+
+
+# package bootstrapping code
+def boot():
+    # bail if {pyre} is running in some special mode
+    if not executive: return
+
+    # get the fileserver
+    fileserver = executive.fileserver
+    # and the nameserver
+    nameserver = executive.nameserver
+
+    # turn the configuration path into a file node
+    cfg = fileserver.local(root=defaults()).discover()
+    # attach it under the {praxis} namespace
+    fileserver['praxis/system'] = cfg
+
+    # get the configuration path
+    cfgpath = nameserver['pyre.configpath']
+
+    # build the uri for the package configuration files
+    uri = executive.uri().coerce("vfs:/praxis/system")
+    # add it to the configuration path
+    cfgpath.append(uri)
+    for x in cfgpath: print(x.uri)
+
+    # create, load and configure the {praxis} package
+    nameserver.package(name='praxis', executive=executive)
+    
+    # all done
+    return
+    
+
+# import and publish pyre symbols
 # protocols, components and traits
 from pyre import schemata, protocol, component, properties
 # decorators
@@ -21,6 +85,9 @@ from pyre import db, records, tabular
 # miscellaneous packages
 from pyre import tracking, units
 
+# fire up
+boot()
+
 # access to the default shells and other components
 from .components import app, dbapp, idd
 # the database schema
@@ -33,16 +100,6 @@ from . import ingest
 from . import support
 # default application bases
 from . import applications
-
-
-# convenience for interactive explorations
-def pg():
-    # build a db component
-    dbc = db.postgres(name="praxis:db")
-    # establish the connection
-    dbc.attach()
-    # and return the component
-    return dbc
 
 
 # end of file 
