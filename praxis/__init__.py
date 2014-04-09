0c865c2f622355ce089900ab6b76b97ca33cddd1
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-04-08 23:00:45 -0700

    praxis: the package bootstrapping now takes advantage of the support in pyre

--- praxis/__init__.py
+++ praxis/__init__.py
@@ -39,41 +39,13 @@ def pg():
     # and return the component
     return dbc
 
-
-# package bootstrapping code
-def boot():
-    # bail if {pyre} is running in some special mode
-    if not executive: return
-
-    # get the fileserver
-    fileserver = executive.fileserver
-    # and the nameserver
-    nameserver = executive.nameserver
-
-    # turn the configuration path into a file node
-    cfg = fileserver.local(root=defaults()).discover()
-    # attach it under the {praxis} namespace
-    fileserver['praxis/system'] = cfg
-
-    # get the configuration path
-    cfgpath = nameserver['pyre.configpath']
-
-    # build the uri for the package configuration files
-    uri = executive.uri().coerce("vfs:/praxis/system")
-    # add it to the configuration path
-    cfgpath.append(uri)
-
-    # all done
-    return
-    
-
 # import and publish pyre symbols
 # protocols, components and traits
 from pyre import schemata, protocol, component, properties
 # decorators
 from pyre import export, provides
 # the runtime manager
-from pyre import executive
+from pyre import executive, addPackageConfiguration
 
 # db support
 from pyre import db, records, tabular
@@ -82,7 +54,7 @@ from pyre import db, records, tabular
 from pyre import tracking, units
 
 # fire up
-boot()
+home, prefix, defaults = addPackageConfiguration(namespace='praxis', file=__file__)
 
 # access to the default shells and other components
 from .components import app, dbapp, idd
