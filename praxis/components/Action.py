dddc0f18aaf26a8da7fa42800a3811a368a48cbf
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-06-16 18:10:41 -0700

    praxis.components: the {Action} protocol now provides better context for name resolution

--- praxis/components/Action.py
+++ praxis/components/Action.py
@@ -17,4 +17,24 @@ class Action(pyre.action, family='praxis.actions'):
     """
 
 
+    @classmethod
+    def pyre_contextPath(cls):
+        """
+        Return an iterable over the starting point for hunting down my actions
+        """
+        # build a string to uri converter
+        uri = cls.uri()
+        # first mine, then the ones i inherit from praxis 
+        return [ uri.coerce(value='vfs:/praxis'), uri.coerce(value='vfs:/pyre') ]
+
+
+    @classmethod
+    def pyre_contextFolders(cls):
+        """
+        Return an iterable over portions of my family name
+        """
+        # spells are in the 'spells' folder
+        return [ 'actions' ]
+
+
 # end of file 
