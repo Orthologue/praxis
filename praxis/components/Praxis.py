7078710334d8f5ef251ce3b1e876c021058e3e79
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-06-16 18:11:41 -0700

    praxis.components: the plexus now provides a help screen and can delegate the construction of the package layout to subclasses

--- praxis/components/Praxis.py
+++ praxis/components/Praxis.py
@@ -26,22 +26,44 @@ class Praxis(pyre.plexus, family='praxis.components.plexus', action=Action):
     jurisdiction.doc = 'compliant calculators for the default company jurisdiction'
 
 
+    # plexus obligations
+    @pyre.export
+    def help(self, **kwds):
+        """
+        Hook for the application help system
+        """
+        # get the package
+        import praxis
+        # show me
+        self.info.log(praxis._praxis_usage)
+        # all done
+        return 0
+
+
     # meta-methods
     def __init__(self, **kwds):
         # chain up
         super().__init__(**kwds)
-
-        # pull in my layout
-        from .Layout import Layout
-        # and attach it
-        self.layout = Layout()
-
+        # build and attach my configuration options
+        self.layout = self.newLayout()
         # all done
         return
 
 
     # implementation details
     layout = None
+
+
+    # initialization hooks
+    def newLayout(self):
+        """
+        Instantiate an object with the global project options
+        """
+        # pull in my layout
+        from .Layout import Layout
+        # and attach it
+        return Layout()
+
 
 
 # end of file 
