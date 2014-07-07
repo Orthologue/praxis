f048872b25f9c4dbf890c50c0ba715110daef212
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-07-07 10:46:11 -0700

    praxis.actions: {debug} can display the table construction statements

--- praxis/actions/Debug.py
+++ praxis/actions/Debug.py
@@ -16,6 +16,10 @@ class Debug(praxis.command, family='praxis.actions.debug'):
     Development and debugging support
     """
 
+    # public state
+    tables = praxis.properties.set(default=None, schema=praxis.properties.str())
+    tables.doc = 'restrict the tables affected to this set'
+
 
     # command obligations
     @praxis.export
@@ -27,6 +31,27 @@ class Debug(praxis.command, family='praxis.actions.debug'):
         self.info.log('debug support')
         self.info.line('usage: {.pyre_namespace} debug <aspect>'.format(plexus))
         self.info.log()
+        # all done
+        return 0
+
+
+    def dbinit(self, plexus, **kwds):
+        """
+        Show me the statements necessary to create my schema
+        """
+        # the schema
+        schema = plexus.schema
+        # and the sql renderer
+        sql = praxis.db.sql()
+        
+        # if the caller didn't specify which tables to build, build them all
+        tables = self.tables or {table.pyre_name for table in schema.tables}
+
+        # go through each table
+        for table in schema.tables:
+            # render
+            for line in sql.createTable(table=table): print(line)
+
         # all done
         return 0
 
