4fca43cfc8874df2762c4c0bf367ec7eea164a5a
Michael Aivazis <michael.aivazis@orthologue.com>
2015-01-05 19:00:48 -0800

    web: fixed a typo in the apache server URL

--- web/apache/Make.mm
+++ web/apache/Make.mm
@@ -18,7 +18,7 @@ all: tidy
 
 RSYNC = /usr/bin/rsync -ruavz --progress --stats
 FILES = praxis
-WEB_USER = root@praxis.orthologue..com
+WEB_USER = root@praxis.orthologue.com
 WEB_LOCATION = $(PACKAGE)
 
 deploy: tidy
